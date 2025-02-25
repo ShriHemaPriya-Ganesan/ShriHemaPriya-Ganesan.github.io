08bb4881d038ace0a58d33f5932d8a4761890fd7
ShriHemaPriya-Ganesan <shrihemapriyaganesan@gmail.com>
2025-02-25 16:54:55 -0400

    updates in CSS

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-
 const navbarToggle = document.getElementById('navbar-toggle');
 const navbarMenu = document.getElementById('navbar-menu');
+const navbarLinks = document.querySelectorAll('.navbar-link');
 
 navbarToggle.addEventListener('click', () => {
     navbarMenu.classList.toggle('active');
 });
 
+navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (navbarMenu.classList.contains('active')) {
+            navbarMenu.classList.remove('active');
+        }
+    });
+});
 window.onload = function () {
     if (window.location.hash) {
         window.location.href = window.location.pathname;
